051e20f6dac9359bddb580e5d86503c90578f63b
Juan F Gonzalez <seminariopv@hotmail.com>
2023-10-14 15:14:52 -0500

    Add function call to gradio

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import whisper
 from audiorecorder import audiorecorder
+from gradio_client import Client
 
 st.title("Team Tonic Demo")
 
@@ -12,6 +13,8 @@ audio = audiorecorder("Click to record audio", "Click to stop recording")
 
 model = whisper.load_model("base")
 st.text("Whisper Model Loaded")
+
+client = Client("https://ysharma-llava-v1.hf.space/--replicas/5hq2h/")
 
 if len(audio) > 0:
     st.audio(audio.export().read())
@@ -26,3 +29,12 @@ if len(audio) > 0:
         st.success("Transcription complete")
         with st.expander("See transcript"):
             st.markdown(result['text'])
+
+        st.text("Sending image and request to the model. Please wait...")
+        result = client.predict(
+            result['text'],
+            image,
+            "Crop",
+            fn_index=7
+        )
+        print(result)
